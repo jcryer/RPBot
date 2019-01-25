39aeffb89b0935719415bb20a5b18262e342a3a5
jcryer <jjcryer@mac.com>
2019-01-25 21:15:32 +0000

    Seems to be working fine.

--- RPBot-Core/RPBot.cs
+++ RPBot-Core/RPBot.cs
@@ -429,7 +429,7 @@ We hope you enjoy your stay!")
                             var roles = new List<DiscordRole>();
                             foreach (var ping in list)
                             {
-                                var pingCut = ping.Substring(2, ping.Length - 3);
+                                var pingCut = ping.Substring(2, ping.Length - 4);
 
                                 if (e.Guild.Roles.Any(x => x.Name == pingCut))
                                 {
@@ -441,7 +441,7 @@ We hope you enjoy your stay!")
                                             await role.UpdateAsync(mentionable: true);
                                             roles.Add(role);
                                         }
-                                        catch { }
+                                        catch (Exception exception) { }
                                     }
                                     content = content.Replace(ping, role.Mention);
                                 }
